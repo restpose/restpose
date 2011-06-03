e698bbf7375c29e4fa615f40b4d62026f0394cea
Richard Boulton <richard@tartarus.org>
2011-06-03 11:49:49 +0100

    Update to display parts of the schema

--- static/static/browse/browse.js
+++ static/static/browse/browse.js
@@ -1,7 +1,57 @@
-$(function() {
-  $("#collections").each(function() {
-    $.get("/colls", function() {
-      console.log(this);
-    });
+function display_obj(obj) {
+  var i, output, value;
+  output = "<ul>";
+  for (i in obj) {
+    output += "<li>" + i;
+    value = obj[i];
+    if (typeof value == "object") {
+      output += display_obj(value);
+    } else {
+      output += ": " + value;
+    }
+    output += "</li>";
+  }
+  output += "</ul>";
+  return output;
+}
+
+function update_coll(target) {
+  var data = target.data();
+  function display_coll(obj) {
+    var i, output, details;
+    output = $("<li>").text(data.coll);
+    details = $("<ul>");
+    details.append($("<li>").text(obj.doc_count + " documents"));
+    details.append($("<li>").text("types").append(display_obj(obj.types)));
+    
+    output.append(details);
+    return output;
+  }
+
+  $.get("/coll/" + data.coll, function(result) {
+    target.children().remove();
+    target.append(display_coll(result));
   });
-});
+}
+
+function update_colllist(target) {
+  function display_colls(obj) {
+    var i, output;
+    output = $("<ul>");
+    for (i in obj) {
+      var item = $("<li><a/></li>");
+      item.data("coll", i)
+          .find("a")
+          .text(i)
+          .attr('href', '#coll/' + i)
+          .click(function() { update_coll(item); });
+      output.append(item);
+    }
+    return output.children();
+  }
+
+  $.get("/coll", function(result) {
+    target.children().remove();
+    target.append(display_colls(result));
+  });
+}
--- static/static/status/status.js
+++ static/static/status/status.js
@@ -1,21 +1,21 @@
-function update_status(target_tasks) {
-  function display_obj(obj) {
-    var i, output, value;
-    output = "<ul>";
-    for (i in obj) {
-      output += "<li>" + i;
-      value = obj[i];
-      if (typeof value == "object") {
-        output += display_obj(value);
-      } else {
-        output += ": " + value;
-      }
-      output += "</li>";
+function display_obj(obj) {
+  var i, output, value;
+  output = "<ul>";
+  for (i in obj) {
+    output += "<li>" + i;
+    value = obj[i];
+    if (typeof value == "object") {
+      output += display_obj(value);
+    } else {
+      output += ": " + value;
     }
-    output += "</ul>";
-    return output;
+    output += "</li>";
   }
+  output += "</ul>";
+  return output;
+}
 
+function update_status(target_tasks) {
   $.get("/status", function(result) {
     target_tasks.html(display_obj(result.tasks));
 
