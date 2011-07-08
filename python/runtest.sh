e9c02f013274fbf1b337b042bdce6559ada0bccf
Richard Boulton <richard@tartarus.org>
2011-07-08 14:24:45 +0100

    Make more readable, and use parameters supplied on command line

--- python/runtest.sh
+++ python/runtest.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-nosetests --with-doctest --logging-filter=-restkit --with-coverage --cover-inclusive --cover-html --cover-erase --cover-tests --cover-package=restpose restpose
+nosetests \
+ --with-doctest \
+ --logging-filter=-restkit \
+ --with-coverage \
+ --cover-inclusive \
+ --cover-html \
+ --cover-erase \
+ --cover-tests \
+ --cover-package=restpose \
+ restpose "$@"
