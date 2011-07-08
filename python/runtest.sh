b5db4677519d9a843a38393487b1aa2091c3a945
Richard Boulton <richard@tartarus.org>
2011-07-08 10:33:17 +0100

    Generate coverage report when running tests

--- python/runtest.sh
+++ python/runtest.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-nosetests --with-doctest --logging-filter=-restkit restpose
+nosetests --with-doctest --logging-filter=-restkit --with-coverage --cover-inclusive --cover-html --cover-erase --cover-tests --cover-package=restpose restpose
