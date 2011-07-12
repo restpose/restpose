b9d164e8daa34b5667986d80e775dae2c9de4bc7
Richard Boulton <richard@tartarus.org>
2011-07-12 08:57:04 +0100

    Update to use RestPoseTestCase, and adjust a few bits of the test to test the current state of support for deleting documents and collections.

--- python/restpose/unittests/index_test.py
+++ python/restpose/unittests/index_test.py
@@ -3,30 +3,21 @@
 # This file is part of the restpose python module, released under the MIT
 # license.  See the COPYING file for more information.
 
-from unittest import TestCase
+from .helpers import RestPoseTestCase
 from .. import Server, ResourceNotFound
 
-class IndexTest(TestCase):
-
-    def wait(self, coll, errors=None):
-        chk = coll.checkpoint().wait()
-        if errors is None:
-            errors = []
-        self.assertEqual(chk.errors, errors)
-        self.assertEqual(chk.total_errors, len(errors))
-        self.assertTrue(chk.reached)
-        self.assertFalse(chk.expired)
+class IndexTest(RestPoseTestCase):
 
     def test_delete_db(self):
         """Test that deleting a database functions correctly.
 
         """
         coll = Server().collection("test_coll")
+        coll.delete()
         doc = { 'text': 'Hello world', 'tag': 'A tag', 'cat': "greeting",
                 'empty': "" }
         coll.add_doc(doc, type="blurb", id="1")
         self.wait(coll)
-        coll.get_doc("blurb", "1")
         self.assertEqual(coll.get_doc("blurb", "1").data,
                          dict(
                               cat = ['greeting'],
@@ -37,23 +28,24 @@ class IndexTest(TestCase):
                               type = ['blurb'],
                              ))
 
+        self.assertTrue('test_coll' in Server().collections)
 
-        return # delete not yet implemented!
         coll.delete()
         self.wait(coll)
+        #self.assertTrue('test_coll' not in Server().collections) # FIXME - disabled currently, because waiting for the checkpoint re-creates the collection.  See docs/todo for a plan to resolve this.
         msg = None
         try:
-            coll.get_doc("blurb", "1")
+            coll.get_doc("blurb", "1").data
         except ResourceNotFound, e:
             msg = e.msg
-        self.assertEqual(msg, 'No collection of name "test_coll" exists')
+        #self.assertEqual(msg, 'No collection of name "test_coll" exists') # FIXME  - disabled for same reason as check of test_coll not being in Server().collections is, a few lines up.
 
     def test_delete_doc(self):
         """Test that deleting a document functions correctly.
 
         """
         coll = Server().collection("test_coll")
-        #coll.delete()
+        coll.delete()
         doc = { 'text': 'Hello world', 'tag': 'A tag', 'cat': "greeting",
                 'empty': "" }
         coll.add_doc(doc, type="blurb", id="1")
@@ -79,18 +71,26 @@ class IndexTest(TestCase):
 
     def test_custom_config(self):
         coll = Server().collection("test_coll")
-        #coll.delete()
+        coll.delete()
+        self.wait(coll)
+        config = coll.config
+        self.assertEqual(config['format'], 3)
+        self.assertEqual(config['types'].keys(), [])
+
         coll.config = {'types': {'foo': {
             'alpha': {'type': 'text'}
         }}}
         self.wait(coll, [{'msg': 'Setting collection config failed with ' +
                   'RestPose::InvalidValueError: Member format was missing'}])
+        self.assertEqual(config['types'].keys(), [])
         coll.config = {'format': 3,
             'types': {'foo': {
                 'alpha': {'type': 'text'}
             }}
         }
-        self.wait(coll, [])
+        self.wait(coll)
         config = coll.config
         self.assertEqual(config['format'], 3)
-        #self.assertEqual(config['types'].keys(), ['foo'])
+        self.assertEqual(config['types'].keys(), ['foo'])
+        coll.delete()
+        self.wait(coll)
