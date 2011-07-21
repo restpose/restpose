9d259337afcbb5db90eb959f7421358d5692f808
Richard Boulton <richard@tartarus.org>
2011-07-21 08:28:20 +0100

    Add a better explanation to the doccomment with doctest lines in it in client.py

--- python/restpose/client.py
+++ python/restpose/client.py
@@ -6,17 +6,26 @@
 """
 Client for RestPose.
 
-Example:
+Servers provide access to a set of collection resources.  A collection can be
 
     >>> from restpose import Server
     >>> server = Server()
-    >>> status = server.status()
     >>> doc = { 'text': 'Hello world', 'tag': 'A tag' }
     >>> coll = server.collection("my_coll")
     >>> coll.add_doc(doc, doc_type="blurb", doc_id="1")
+
+After making changes, we use a checkpoint to cause the changes to be applied
+immediately, and to wait until they have been applied.  The changes will be
+applied after a short period of inactivity (by default, 5 seconds) even if we
+don't use a checkpoint.
+
     >>> checkpt = coll.checkpoint().wait()
     >>> checkpt.total_errors, checkpt.errors, checkpt.reached
     (0, [], True)
+
+Once the changes have been applied, we can perform a search by building a
+query.
+
     >>> query = coll.doc_type("blurb").field_is('tag', 'A tag')
     >>> query.matches_estimated
     1
