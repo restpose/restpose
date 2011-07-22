41afabac727e791b9da627df5d5fbf6d77b45a61
Richard Boulton <richard@tartarus.org>
2011-07-22 11:20:54 +0100

    Remove python client - now in separate restpose-py repository

--- python/covertests.sh
+++
@@ -1,4 +0,0 @@
-# Run tests with condition coverage.
-# Requires the python package "instrumental" to be installed.
-
-instrumental -rs -t restpose `which nosetests`
--- python/docs/overview.rst
+++
@@ -1,5 +0,0 @@
-Overview
-========
-
-.. todo:: Write overview to restpose python client.
-
